62b172dc509fbc07e61ebb10b453776811534b86
Balázs Gregorics <grego.balu@gmail.com>
2017-08-17 20:05:51 +0200

    Added basic frame editor project

---
+++ Leviathan.Editors.Frame/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Leviathan.Editors.Frame
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CampaignButton = new System.Windows.Forms.Button();
+            this.RaceButton = new System.Windows.Forms.Button();
+            this.ClassButton = new System.Windows.Forms.Button();
+            this.WorkspaceTextBox = new System.Windows.Forms.TextBox();
+            this.ChangeWorkspaceButton = new System.Windows.Forms.Button();
+            this.CharacterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CampaignButton
+            // 
+            this.CampaignButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CampaignButton.Location = new System.Drawing.Point(12, 12);
+            this.CampaignButton.Name = "CampaignButton";
+            this.CampaignButton.Size = new System.Drawing.Size(220, 30);
+            this.CampaignButton.TabIndex = 0;
+            this.CampaignButton.Text = "Campaign Editor";
+            this.CampaignButton.UseVisualStyleBackColor = true;
+            this.CampaignButton.Click += new System.EventHandler(this.CampaignButton_Click);
+            // 
+            // RaceButton
+            // 
+            this.RaceButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RaceButton.Location = new System.Drawing.Point(12, 48);
+            this.RaceButton.Name = "RaceButton";
+            this.RaceButton.Size = new System.Drawing.Size(220, 30);
+            this.RaceButton.TabIndex = 1;
+            this.RaceButton.Text = "Race Editor";
+            this.RaceButton.UseVisualStyleBackColor = true;
+            this.RaceButton.Click += new System.EventHandler(this.RaceButton_Click);
+            // 
+            // ClassButton
+            // 
+            this.ClassButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ClassButton.Location = new System.Drawing.Point(12, 84);
+            this.ClassButton.Name = "ClassButton";
+            this.ClassButton.Size = new System.Drawing.Size(220, 30);
+            this.ClassButton.TabIndex = 2;
+            this.ClassButton.Text = "Class Editor";
+            this.ClassButton.UseVisualStyleBackColor = true;
+            this.ClassButton.Click += new System.EventHandler(this.ClassButton_Click);
+            // 
+            // WorkspaceTextBox
+            // 
+            this.WorkspaceTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WorkspaceTextBox.Enabled = false;
+            this.WorkspaceTextBox.Location = new System.Drawing.Point(12, 229);
+            this.WorkspaceTextBox.Name = "WorkspaceTextBox";
+            this.WorkspaceTextBox.Size = new System.Drawing.Size(220, 20);
+            this.WorkspaceTextBox.TabIndex = 3;
+            // 
+            // ChangeWorkspaceButton
+            // 
+            this.ChangeWorkspaceButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ChangeWorkspaceButton.Location = new System.Drawing.Point(12, 200);
+            this.ChangeWorkspaceButton.Name = "ChangeWorkspaceButton";
+            this.ChangeWorkspaceButton.Size = new System.Drawing.Size(115, 23);
+            this.ChangeWorkspaceButton.TabIndex = 4;
+            this.ChangeWorkspaceButton.Text = "Change Workspace";
+            this.ChangeWorkspaceButton.UseVisualStyleBackColor = true;
+            this.ChangeWorkspaceButton.Click += new System.EventHandler(this.ChangeWorkspaceButton_Click);
+            // 
+            // CharacterButton
+            // 
+            this.CharacterButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CharacterButton.Location = new System.Drawing.Point(12, 120);
+            this.CharacterButton.Name = "CharacterButton";
+            this.CharacterButton.Size = new System.Drawing.Size(220, 30);
+            this.CharacterButton.TabIndex = 5;
+            this.CharacterButton.Text = "Character Editor";
+            this.CharacterButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 261);
+            this.Controls.Add(this.CharacterButton);
+            this.Controls.Add(this.ChangeWorkspaceButton);
+            this.Controls.Add(this.WorkspaceTextBox);
+            this.Controls.Add(this.ClassButton);
+            this.Controls.Add(this.RaceButton);
+            this.Controls.Add(this.CampaignButton);
+            this.MinimumSize = new System.Drawing.Size(260, 300);
+            this.Name = "MainForm";
+            this.Text = "Leviathan Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CampaignButton;
+        private System.Windows.Forms.Button RaceButton;
+        private System.Windows.Forms.Button ClassButton;
+        private System.Windows.Forms.TextBox WorkspaceTextBox;
+        private System.Windows.Forms.Button ChangeWorkspaceButton;
+        private System.Windows.Forms.Button CharacterButton;
+    }
+}
+
---
+++ Leviathan.Editors.Frame/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Leviathan.Editors.Frame
+{
+    public partial class MainForm : Form
+    {
+        private String _workspaceDir;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            WorkspaceTextBox.Text = "None...";
+        }
+
+        private void CampaignButton_Click(object sender, EventArgs e)
+        {
+            CampaignEditor.MainForm mf = new CampaignEditor.MainForm();
+            mf.ShowDialog();
+        }
+
+        private void RaceButton_Click(object sender, EventArgs e)
+        {
+            RaceEditor.MainForm mf = new RaceEditor.MainForm();
+            mf.ShowDialog();
+        }
+
+        private void ClassButton_Click(object sender, EventArgs e)
+        {
+            ProfessionEditor.MainForm mf = new ProfessionEditor.MainForm();
+            mf.ShowDialog();
+        }
+
+        private void ChangeWorkspaceButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Select workspace directory";
+            DialogResult dr = fbd.ShowDialog();
+
+            if (dr != DialogResult.OK)
+                return;
+
+            _workspaceDir = fbd.SelectedPath;
+            WorkspaceTextBox.Text = _workspaceDir;
+        }
+    }
+}
